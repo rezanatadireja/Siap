8f9c30fd659e29edd91011fff815dfe5e6750ac0
RFF <rezafeisalfajri@gmail.com>
2022-10-10 23:58:01 +0700

    pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,5 +36,6 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    forceTLS: true,
+    authEndpoint: "/broadcasting/auth",
 });
